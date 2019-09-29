f698bc46c1ddb7d108fb8b2f324f7b37f69ddb23
Morgan Dean <morgandeanhi@gmail.com>
2019-09-28 14:09:56 -1000

    boostrap

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+
 import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(Router);
+Vue.use(BootsrapVue);
+
 
 export default new Router({
   mode: 'history',
