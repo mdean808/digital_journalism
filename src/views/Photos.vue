9054a880ff957b49560c59137e06b7b38b9a9258
Morgan Dean <morgandeanhi@gmail.com>
2019-09-29 20:22:43 -1000

    many changes

---
+++ src/views/Photos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-container>
+        <b-row>
+            <Box hover color="#ad8e30"/>
+        </b-row>
+    </b-container>
+    </div>
+</template>
+
+<script>
+    import Box from '../components/Box'
+	export default {
+		name: "Videos",
+        components: {
+			Box
+        }
+	}
+</script>
+
+<style scoped>
+
+</style>
